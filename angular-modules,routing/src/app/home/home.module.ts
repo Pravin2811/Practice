a17efef8a753c6c1eaf9093c64da3451aaed6fcd
Pravin2811 <cpravinkumar007@gmail.com>
2024-04-23 17:46:51 +0530

    practice

---
+++ angular-modules,routing/src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeMainComponent } from './home-main/home-main.component';
+
+
+@NgModule({
+  declarations: [
+    HomeMainComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ],
+  exports:[
+  ]
+})
+export class HomeModule { }
